6b47b9ae3bf2557ca54f2e7abcfbe28f95014bb7
I528715 <karuna.sharma@sap.com>
2025-03-12 16:42:06 +0530

    redirect uri modified

--- ASTRA/Dashboard/src/utils/authConfig.ts
+++ ASTRA/Dashboard/src/utils/authConfig.ts
@@ -3,6 +3,9 @@ export const authConfig = {
   clientId: "ae1b718c-172b-4800-b72a-5daaa2a4952b",
   clientSecret: "3dAbrH9Yw=rfD_7YVf-RJdg34_HEqsv",
   redirectUri: "http://localhost:5173/login",
+  redirectUri: process.env.NODE_ENV === 'production' 
+    ? "https://astra-daring-gelada-kk.cfapps.us10-001.hana.ondemand.com/login" 
+    : "http://localhost:5173/login",
   userInfoUrl: "https://aztwzrclb.trial-accounts.ondemand.com/oauth2/userinfo",
   authUrl: "https://aztwzrclb.trial-accounts.ondemand.com/oauth2/authorize",
   logoutUrl: "https://aztwzrclb.trial-accounts.ondemand.com/oauth2/logout"
